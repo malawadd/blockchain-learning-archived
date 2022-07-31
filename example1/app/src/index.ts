a50845aa14174b0fe428c6a6eb849c1a861a51be
malawadd <50057882+malawadd@users.noreply.github.com>
2022-07-31 14:47:49 +0300

    contract-dapp

---
+++ example1/app/src/index.ts
@@ -0,0 +1,64 @@
+import * as p5 from 'p5';
+import { connect } from "@tableland/sdk";
+
+// The points from tableland we'll need to render
+let points: Array<{x: number, y: number, id: Number}> = [];
+// The current token id. pulled from http://url/#{id}
+let tokenId = +window.location.hash.substr(1).replace("/", "")
+// Some anchors in the canvas to help offset and scale the points
+let anchors: Array<number> = [];
+// A function we call on each canvas render or resize to recalc anchors
+const updateAnchors = (w: number, h: number) => {
+    let dmin = Math.min(w, h) - 8; // 8 just gives some buffer
+    // How to scale raw x,y for our token. recall, x & y are between 0 and 512.
+    let scalar = dmin / 512;
+    anchors = [
+        (w / 2), // Screen midpoint
+        (h / 2), // Screen midpoint
+        scalar
+    ]
+}
+
+export const sketch = (p: p5) => {
+    p.windowResized = () => {
+        updateAnchors(window.innerWidth, window.innerHeight);
+        p.resizeCanvas(window.innerWidth, window.innerHeight);
+    }
+    p.setup = () => {
+        updateAnchors(window.innerWidth, window.innerHeight);
+        p.createCanvas(window.innerWidth, window.innerHeight);
+    
+        // Connect to the Tableland network
+        connect({ network: "testnet" })
+        .then(connection => {
+            // Run a SQL select on our project table
+            return connection.read("select * from canvas_5_4")
+        })
+        .then(data => {
+            // Format and store our data in the points[] array
+            points = data.rows
+            .map((d, id) => ({x: d[2], y: d[3], id}))
+        })
+    }
+    
+
+    p.draw = () => {
+        // black background
+        p.background(0);
+        for (let pt of points) {
+            // Magenta
+            p.fill(255, 51, 255, 155);
+            if (pt.id === tokenId) {
+                // Aqua
+                p.fill(0, 178, 255, 211);
+            }
+            p.noStroke();
+            // Use the anchor and scalar to render a square space
+            let nx = ((pt.x - 256) * anchors[2]) + anchors[0];
+              let ny = ((pt.y - 256) * anchors[2]) + anchors[1];
+            p.ellipse(nx, ny, 10 * anchors[2], 10 * anchors[2]);
+      }
+    }
+}
+
+export const myp5 = new p5(sketch, document.body);
--- example1/contracts/Canvas.sol
+++ example1/contracts/Canvas.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "@tableland/evm/contracts/ITablelandTables.sol";
 
+import "@tableland/evm/contracts/ITablelandController.sol";
+
 contract Canvas is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -14,8 +16,46 @@ contract Canvas is ERC721URIStorage, Ownable {
     // Our will be pulled from the network
     string private _baseURIString =
         "https://testnet.tableland.network/query?s=";
+    
+    ITablelandTables private _tableland;
+    string private _metadataTable;
+    uint256 private _metadataTableId;
+    string private _tablePrefix = "canvas"; 
 
-    constructor() ERC721("Cavas", "CVS") {}
+    constructor(
+        address registry,
+    ) ERC721("Cavas", "CVS") {
+        /* 
+      * The Tableland address on your current chain
+      */
+      _tableland = ITablelandTables(registry);
+
+      /*
+      * Stores the unique ID for the newly created table.
+      */
+      _metadataTableId =  _tableland createTable(
+        address(this),
+        string concat(
+            "CREATE TABLE",
+            _tablePrefix,
+            Strings.toString(block.chainid),
+            "(id int, external_link text, x int, y int);"
+        )
+      );
+
+      /*
+      * Stores the full tablename for the new table. 
+      * {prefix}_{chainid}_{tableid}
+      */
+
+        _metadataTable = string concat(
+            _tablePrefix,
+            "_",
+            Strings.toString(block.chainid),
+            "_",
+            Strings.toString(_metadataTableId)
+        );
+    }
 
     /*
      * @dev safeMint allows anyone to mint a token in this project.
@@ -25,12 +65,37 @@ contract Canvas is ERC721URIStorage, Ownable {
 
     function safeMint(address to) public returns (uint256) {
         uint256 newItemId = _tokenIds.current();
+        _tableland.runSQL(
+            address(this),
+            _metadataTable,
+            string concat(
+                "INSERT INTO",
+                _metadataTable,
+                "(id, external_link, x, y) VALUES (",
+                Strings.toString(newItemId),
+                ", 'not.implemented.xyz', 0, 0);"
+            )
+        )
 
         /* Any table updates will go here */
 
         _safeMint(to, newItemId, "");
         _tokenIds.increment();
         return newItemId;
+    }
+
+    /*
+     * @dev create a read method on your smart contract to get the final table
+     * name in order to query ii
+     */
+
+    function metadataURI() public view returns (string memory) {
+        string memory base = _baseURI();
+        return string concat(
+            base, 
+            "SELECT%20*%20FROM%20",
+            _metadataTable
+        );
     }
 
     /*
@@ -46,12 +111,28 @@ contract Canvas is ERC721URIStorage, Ownable {
         uint256 y
     ) public {
         require(this.ownerOf(tokenId) == msg.sender, "Invalid owner");
-        require(tokenId < _tokenIds.current());
+
 
         require(x < 512 && 0 <= x, "Out of bounds");
         require(y < 512 && 0 <= y, "Out of bounds");
 
-        /* Any table updates will go here */
+        // update the row in tableland
+        _tableland.runSQL(
+            address(this),
+            _metadataTable,
+            string concat(
+                "UPDATE",
+                _metadataTable,
+                "SET x = ",
+                Strings.toString(x),
+                ", y = ",
+                Strings.toString(y),
+                "WHERE id = ",
+                Strings.toString(tokenId),
+                ";"
+            )
+        );
+
     }
 
     function _baseURI() internal view override returns (string memory) {
@@ -81,4 +162,29 @@ contract Canvas is ERC721URIStorage, Ownable {
         /* We will give token viewers a way to get at our table metadata */
         return;
     }
+
+    function getPolicy(address sender) public payable override returns (ITablelandController.Policy memory) {
+        
+         /*
+      * Add any custom ACL check here.
+      */
+        
+        return 
+        ITablelandController.Policy(
+            allowInsert: true,
+            allowUpdate: false,
+            allowDelete: false,
+            whereClause: Policies.joinClauses(new string[](0)),
+            withCheck: Policies.joinClauses(new string[](0)),
+            updatableColumns: new string[](0)
+        );
+    }
+
+    function updateController() onlyOwner {
+        _tableland.setController(
+           address(this),
+           _tableId,
+           address(this)
+         );
+     }
 }
